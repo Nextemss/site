ae1d86c40193bc4d8084d52c5095faa4c552283c
Nextems <108946936+Nextemss@users.noreply.github.com>
2023-04-02 13:31:21 +0300

    Add files via upload

---
+++ front/register/registration.js
@@ -0,0 +1,28 @@
+const form = document.getElementById('registration-form');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const username = document.getElementById('username').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const confirmPassword = document.getElementById('confirm-password').value;
+
+  if (password !== confirmPassword) {
+    alert('Passwords do not match!');
+    return;
+  }
+
+  const response = await fetch('/main/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ username, email, password })
+  });
+
+  const result = await response.json();
+  console.log(result);
+
+  alert('Registration successful!');
+});
